47854e63853c3b224949137c36688ccff068fe3b
BIT\BITCAMP <ponde119@outlook.com>
2024-06-12 10:59:47 +0900

    24.06.12 이준섭 백준알고리즘 9461번(파도반 수열 - dp) 문제 풀이 for Java

---
+++ src/pro9000/Problem_9461.java
@@ -0,0 +1,32 @@
+package pro9000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_9461 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		final int T = Integer.parseInt(br.readLine());
+		final int MAX_SIZE = 100;
+		long[] padovan = new long[MAX_SIZE+1];
+		
+		padovan[1] = 1;
+		padovan[2] = 1;
+		padovan[3] = 1;
+		padovan[4] = 2;
+		padovan[5] = 2;
+		
+		for(int i = 6; i <= MAX_SIZE; ++i) {
+			padovan[i] = padovan[i-1] + padovan[i-5];
+		}
+		
+		// 출력
+		for(int i = 1; i <= T; ++i) {
+			System.out.println(padovan[Integer.parseInt(br.readLine())]);
+		}
+		
+		br.close();
+	}
+}
