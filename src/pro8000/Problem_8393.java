a49ededf24b5323ca2cf788900265e9a33d4b741
sseopsseop <ponde119@outlook.com>
2024-06-16 23:34:31 +0900

    24.06.16 이준섭 합 - 8393(구현, 수학) 문제 풀이 for Java

---
+++ src/pro8000/Problem_8393.java
@@ -0,0 +1,20 @@
+package pro8000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_8393 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		int sum = 0;
+		for(int i = 1; i<=n; ++i) {
+			sum += i;
+		}
+		
+		System.out.println(sum);
+	}
+}
