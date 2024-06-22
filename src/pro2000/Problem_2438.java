c10d95b7d0cbedb94f91cbfc9a141af0b9132f0c
sseopsseop <ponde119@outlook.com>
2024-06-23 03:03:15 +0900

    24.06.23 이준섭 백준 알고리즘 : 별 찍기 - 1 - 2438(구현) 문제 풀이 for Java

---
+++ src/pro2000/Problem_2438.java
@@ -0,0 +1,22 @@
+package pro2000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2438 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 1; i <= n ; ++i) {
+			for(int j = 1; j <= i; ++j) {
+				System.out.print('*');
+			}
+			System.out.println();
+		}
+		
+		br.close();
+	}
+}
