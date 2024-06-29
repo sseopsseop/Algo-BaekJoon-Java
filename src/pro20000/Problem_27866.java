07f62793ec2a5eeb0067da02e390a7d98ab1e63a
sseopsseop <ponde119@outlook.com>
2024-06-29 20:50:34 +0900

    24.06.29 이준섭 백준 알고리즘 : [Bronze V] 문자와 문자열 - 27866(구현, 문자열) 문제풀이 for Java

---
+++ src/pro20000/Problem_27866.java
@@ -0,0 +1,21 @@
+package pro20000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_27866 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String text = br.readLine();
+		int i = Integer.parseInt(br.readLine());
+		
+		StringBuilder res = new StringBuilder();
+		
+		res.append(text.charAt(i-1));
+		
+		System.out.println(res.toString());
+	}
+
+}
