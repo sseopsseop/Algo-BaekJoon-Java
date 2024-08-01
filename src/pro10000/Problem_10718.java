903c512f51309e6b4e1ed50e4977c9ceec1d8905
sseopsseop <ponde119@outlook.com>
2024-08-01 23:10:48 +0900

    24.08.01 이준섭 백준 알고리즘 [Bronze V] We love kriii - 10718 구현 문제풀이 for Java

---
+++ src/pro10000/Problem_10718.java
@@ -0,0 +1,12 @@
+package pro10000;
+
+import java.io.IOException;
+
+public class Problem_10718 {
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
+		
+	}
+}
