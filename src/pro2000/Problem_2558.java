1478ec3d458e775813c4f83a7cad11ebfaec3490
sseopsseop <ponde119@outlook.com>
2024-08-11 22:28:04 +0900

    24.08.11 이준섭 백준 알고리즘 [Bronze V] A+B - 2 - 2558 구현, 사칙연산, 수학 문제풀이 for Java

---
+++ src/pro2000/Problem_2558.java
@@ -0,0 +1,17 @@
+package pro2000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2558 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.println(a+b);
+		br.close();
+	}
+}
